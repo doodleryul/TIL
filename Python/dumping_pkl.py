f12e275f23396b2f9ed66153e44b19252a93df09
율링 <76838077+rimiiii@users.noreply.github.com>
2021-02-02 12:22:05 +0900

    Create dumping_pkl.py

---
+++ Python/dumping_pkl.py
@@ -0,0 +1,7 @@
+
+def dumping_pkl(filePath, outFilename, files):
+    import os, pickle
+    dumpingPath = os.path.join(filePath, outFilename)
+    print("Dumping at..", dumpingPath)
+    with open(dumpingPath, 'wb') as outp:
+        pickle.dump(files, outp, -1)
